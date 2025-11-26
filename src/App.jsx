c5552e0e7037e3215a90e52c049c1c2d628b4d19
zander <zstraight82@gmail.com>
2025-11-26 15:54:39 -0500

    Added custom animated cursors

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,20 @@ import './App.css'
 import SideNav from './components/SideNav/SideNav.jsx';
 import Header from './components/Header/Header.jsx';
 import { Outlet } from 'react-router-dom';
+import { useEffect } from 'react';
 
 function App() {
+    useEffect(() => {
+        const img1 = new Image(); img1.src = '/cursor/hand1.png';
+        const img2 = new Image(); img2.src = '/cursor/hand2.png';
+        const img3 = new Image(); img3.src = '/cursor/point1.png';
+        const img4 = new Image(); img4.src = '/cursor/point2.png';
+        
+        const intervalId = setInterval(() => {
+            document.body.classList.toggle('cursor-toggle');
+        }, 100);
+        return () => clearInterval(intervalId);
+    }, []);
 
     return (
         <>
