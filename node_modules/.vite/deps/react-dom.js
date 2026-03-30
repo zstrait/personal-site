719e09a02ff842273d1046a7942906648aa44dc5
zander <zstraight82@gmail.com>
2026-03-30 16:34:30 -0400

    stop tracking node_modules

--- node_modules/.vite/deps/react-dom.js
+++
@@ -1,5 +0,0 @@
-import {
-  require_react_dom
-} from "./chunk-ZEJDETLQ.js";
-import "./chunk-AQI5YXZE.js";
-export default require_react_dom();
--- node_modules/.vite/deps/react.js
+++
@@ -1,4 +0,0 @@
-import {
-  require_react
-} from "./chunk-AQI5YXZE.js";
-export default require_react();
