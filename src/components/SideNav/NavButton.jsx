b2ecce4ad58cbca2cb0e5b8dad2cebd0a1bc0894
zander <zstraight82@gmail.com>
2026-07-25 22:19:00 -0400

    Added functionality to scroll page to top when clicking active navigation link

--- src/components/SideNav/NavButton.jsx
+++ src/components/SideNav/NavButton.jsx
@@ -1,9 +1,24 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
+
 
 function NavButton({ label, link }) {
+    const location = useLocation();
+
+    const handleClick = () => {
+        if (location.pathname === link) {
+            const contentView = document.querySelector('.content-view');
+            if (contentView) {
+                contentView.scrollTo({
+                    top: 0,
+                    behavior: 'smooth'
+                });
+            }
+        }
+    };
+
     return (
         <li>
-            <Link to={link} className={`nav-button ${label}-button`}></Link>
+            <Link to={link} onClick={handleClick} className={`nav-button ${label}-button`}></Link>
         </li>
     );
 }
