71187d5193cc15184a85dedfe012fde74400bf54
zander <zstraight82@gmail.com>
2026-08-02 05:12:51 -0400

    Updated socials link

--- src/components/Header/SocialsButton.jsx
+++ src/components/Header/SocialsButton.jsx
@@ -27,7 +27,7 @@ function Socialsbutton() {
                 <div className={`socials-bar ${isSocialsOpen ? "open" : ""}`}>
                     <a className="socials-icon flickr" href="https://www.flickr.com/photos/zstrt/" target="_blank"></a>
                     <a className="socials-icon letterboxd" href="https://letterboxd.com/elliottsmiff/" target="_blank"></a>
-                    <a className="socials-icon instagram" href="https://www.instagram.com/zanderstrt/" target="_blank"></a>
+                    <a className="socials-icon instagram" href="https://www.instagram.com/zander.strt/" target="_blank"></a>
                     <a className="socials-icon spotify" href="https://open.spotify.com/user/47g2z8i6bncxlzas8mqf222fb?si=f95d03d3354047b8" target="_blank"></a>
                     <a className="socials-icon linkedin" href="https://www.linkedin.com/in/zander-strait/" target="_blank"></a>
                     <a className="socials-icon github" href="https://github.com/zstrait" target="_blank"></a>
